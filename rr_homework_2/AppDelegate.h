5809ef6f37e556ffad9fbd694014671053e2e051
Rob Rivera <robert.rivera@t8webware.com>
2012-05-31 15:10:04 -0500

    Initial Commit

---
+++ rr_homework_2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rr_homework_2
+//
+//  Created by Robert Rivera on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rr_homework_2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rr_homework_2
+//
+//  Created by Robert Rivera on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
