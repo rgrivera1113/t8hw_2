bbf3bcf31bfed925b73a9fdac447a0f7ecf5414d
Rob Rivera <robert.rivera@t8webware.com>
2012-06-07 15:16:37 -0500

    Changed code to not try a file write to cache if the image already exists.

--- rr_homework_2/FlickrCacher.m
+++ rr_homework_2/FlickrCacher.m
@@ -16,6 +16,7 @@
     NSArray* directory = [fm URLsForDirectory: NSCachesDirectory inDomains:NSUserDomainMask];
     NSURL* filePath = [[directory objectAtIndex:0] URLByAppendingPathComponent:@"images" isDirectory:YES];
     
+    // Create the images directory in the cache directory if it doesn't exist.
     NSError* error;
     if (![fm fileExistsAtPath:filePath.path])
         [fm createDirectoryAtURL:filePath withIntermediateDirectories:NO attributes:NULL error:&error];
@@ -25,7 +26,12 @@
     // Check the size of the cache directory.
     // Remove the oldest photo in the cache, if necessary.
     // Write the new file to the cache.
-    [fm createFileAtPath:filePath.path contents:photo attributes:NULL];
+    if (![fm fileExistsAtPath:filePath.path]) {
+        NSLog(@"Image doesn't exist in cache.");
+        [fm createFileAtPath:filePath.path contents:photo attributes:NULL];
+    } else {
+        NSLog(@"Image already exists.  Don't add.");
+    }
     
 }
 
